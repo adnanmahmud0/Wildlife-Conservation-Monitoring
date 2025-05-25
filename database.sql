b7a70204076c80f1534a679d03b05ce284908b02
adnanmahmud0 <adnan99mahmud@gmail.com>
2025-05-25 20:18:58 +0600

    problem 2 sloved

--- database.sql
+++ database.sql
@@ -52,3 +52,6 @@ INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VA
 -- Problem 1: Register a new ranger
  INSERT INTO rangers (name, region) VALUES ('Derek Fox', 'Coastal Plains');
 
+
+-- Problem 2: Count unique species ever sighted
+SELECT COUNT (DISTINCT species_id) AS unique_species_count FROM sightings;
